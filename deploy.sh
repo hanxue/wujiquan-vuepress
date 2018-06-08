bf55dbc4457339df728beccd57752a779456e6bc
Lee Hanxue <hanxue.developer@gmail.com>
2018-06-08 16:20:55 +0800

    Add publishing to Github pages
    URL is https://hanxue.github.io/wujiquan-vuepress/
    
    The steps taken are:
    1. Delete existing docs in docs/.vuepress/dist
    2. Generate new docs
    3. Go to docs/.vuepress/dist , initialize a git repo
    4. Force push contents to gh-pages branch of this repo
    5. Github automagically update content at https://hanxue.github.io/wujiquan-vuepress/

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,24 @@
 set -e
 
 # build
+rm -rf docs/.vuepress/dist
 yarn run build
 
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'v.wujiquan.net' > CNAME
+# echo 'v.wujiquan.net' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
+isodate=`date -u +"%Y-%m-%d %H-%M-%SZ"`
+git commit -m "[vuepress] last generated on $isodate"
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:hanxue/wujiquan-vuepress.git master:gh-pages
 
 cd -
